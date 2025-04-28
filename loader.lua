819b2cf1fb4080153cd2c7410ee4389996e6afe0
salinto <david.aco777@icloud.com>
2025-04-28 19:15:10 +0200

    loader.lua

--- loader.lua
+++ loader.lua
@@ -2,5 +2,24 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
--- Your loader script starts here
-print("Loader executed successfully")
+local yena = getgenv().yena
+
+if not yena then
+    return warn("[Yena Loader] No config found!")
+end
+
+if yena.Aimbot and yena.Aimbot.Enabled then
+    print("[Aimbot] Enabled - HitPart: " .. yena.Aimbot.HitPart)
+end
+
+if yena.SilentAim and yena.SilentAim.Enabled then
+    print("[SilentAim] Enabled")
+end
+
+if yena.TriggerBot and yena.TriggerBot.Enabled then
+    print("[TriggerBot] Enabled")
+end
+
+if yena.Movement and yena.Movement.Enabled then
+    print("[Movement] Enabled - Speed: " .. yena.Movement.WalkSpeed.Speed.Modified)
+end
